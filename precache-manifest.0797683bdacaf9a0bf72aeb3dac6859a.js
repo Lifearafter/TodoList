eac1067de3161506511d54ae8e03725f7fc7818c
Muhammad Zaid <mxz190024@utdallas.edu>
2022-09-19 18:29:59 -0500

    Updates

---
+++ precache-manifest.0797683bdacaf9a0bf72aeb3dac6859a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "510212ea19a11e191204",
+    "url": "/TodoList/static/css/main.69f34843.chunk.css"
+  },
+  {
+    "revision": "510212ea19a11e191204",
+    "url": "/TodoList/static/js/main.aff77eca.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  },
+  {
+    "revision": "e1e9a16cd4d9d9411ec9",
+    "url": "/TodoList/static/js/2.08cba4b5.chunk.js"
+  },
+  {
+    "revision": "d7a70855f4c32e61df36b481fdc22f2e",
+    "url": "/TodoList/index.html"
+  }
+];
